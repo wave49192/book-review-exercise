6a3d63aa6ba9f7c3a2cf3452f22aa70cd8347973
wavesirawich <sirawich.t@datawow.io>
2024-08-27 18:00:53 +0700

    migrate: add null: false constraint to name and description in book columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_27_032253) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_27_100934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+    t.string "name", null: false
+    t.text "description", null: false
     t.date "release"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "star"
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
+  add_foreign_key "reviews", "books"
 end
