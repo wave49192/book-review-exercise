5296de13c971c7b54c5937b7d59e9b8468d7d532
wavesirawich <sirawich.t@datawow.io>
2024-08-27 12:01:37 +0700

    generate reviews controller, model and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root "books#index"
-  resources :books
+  resources :books do
+    resources :reviews, only: [ :create, :update, :destroy, :edit ]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
