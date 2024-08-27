3182f2c2870563c5ff5d6c54a31ed7e91debe825
wavesirawich <sirawich.t@datawow.io>
2024-08-27 11:48:04 +0700

    fix white spacing convenction

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  validates :name, presence:true
-  validates :description, presence:true, length:  {minimum: 12}
+  validates :name, presence: true
+  validates :description, presence: true, length:  { minimum: 12 }
 end
