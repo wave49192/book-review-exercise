65d8778c6f3215e89f72c8040e6883de0f9e171e
wavesirawich <sirawich.t@datawow.io>
2024-08-28 10:07:53 +0700

    migrate: add null: false constraint and default value to columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_27_045446) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_28_025757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+    t.string "name", null: false
+    t.text "description", null: false
     t.date "release"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "comment"
-    t.integer "star"
+    t.string "comment", null: false
+    t.integer "star", default: 0, null: false
     t.bigint "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
