ca73c466d33a376662a00a1acc0b105f6380b50d
wavesirawich <sirawich.t@datawow.io>
2024-09-02 17:27:46 +0700

    fix: fix logic to handle flexible items per page param

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
   before_action :authenticate_user!
   def index
-    @books = Book.order(:name).page(params[:page]).per(3)
+    @books = Book.order(:name).page(params[:page]).per(params[:per_page] || 3)
   end
 
   def show
