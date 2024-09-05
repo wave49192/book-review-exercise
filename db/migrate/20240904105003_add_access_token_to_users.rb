98f3411817c5e3ede25917fe87ef608a00d5a8b5
wavesirawich <sirawich.t@datawow.io>
2024-09-05 14:35:35 +0700

    chore: remove old unused migration field

--- db/migrate/20240904105003_add_access_token_to_users.rb
+++ db/migrate/20240904105003_add_access_token_to_users.rb
@@ -1,6 +1,5 @@
 class AddAccessTokenToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :access_token, :string
-    remove_column :users, :authentication_token, :string
   end
 end
