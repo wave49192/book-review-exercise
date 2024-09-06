6a92991a187b10cbd89266114fa02adfd5990927
wavesirawich <sirawich.t@datawow.io>
2024-09-06 15:39:45 +0700

    feat: implement cache with redis

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,26 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
   before_action :authenticate_user!
+  after_action :clear_cache, only: %i[create update destroy]
 
   def index
     page = params[:page].to_i.positive? ? params[:page] : 1
     per_page = params[:per_page].to_i.positive? ? params[:per_page] : 3
-    @books = Book.order(:name).page(page).per(per_page)
+    cache_key = "books/page/#{page}/per_page/#{per_page}"
+
+    @books = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+      Book.order(:name).all.to_a
+    end
+
+    @books = Kaminari.paginate_array(@books).page(page).per(per_page)
   end
 
+
   def show
-    @reviews = @book.reviews
+    cache_key = "book/#{@book.id}/reviews"
+    @reviews = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+      @book.reviews.to_a
+    end
     @review = Review.new
   end
 
@@ -21,7 +32,7 @@ class BooksController < ApplicationController
   end
 
   def create
-   @book = Book.new(book_params)
+    @book = Book.new(book_params)
     if @book.save
       redirect_to @book
     else
@@ -43,6 +54,11 @@ class BooksController < ApplicationController
   end
 
   private
+
+  def clear_cache
+    Rails.cache.delete_matched("books/page/*/per_page/*")
+    Rails.cache.delete("book/#{@book.id}/reviews") if @book.present?
+  end
 
   def set_book
     @book ||= Book.find(params[:id])
